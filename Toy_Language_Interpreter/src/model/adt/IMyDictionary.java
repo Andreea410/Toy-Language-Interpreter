742cafe7ebddbd0d2c387f6b137bdd17fb943445
Andreea410 <andreeaungur494@yahoo.com>
2024-10-24 14:15:56 +0300

    fixed MyDictionary functions and implemented the remove

--- Toy_Language_Interpreter/src/model/adt/IMyDictionary.java
+++ Toy_Language_Interpreter/src/model/adt/IMyDictionary.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public interface IMyDictionary<K,V>
 {
     void insert(K key , V value);
-    V getValue(K key);
+    V getValue(K key) throws KeyNotFoundException;
     void remove(K key) throws KeyNotFoundException;
     boolean contains(K key);
 }
--- Toy_Language_Interpreter/src/model/adt/MyDictionary.java
+++ Toy_Language_Interpreter/src/model/adt/MyDictionary.java
@@ -7,10 +7,6 @@ import java.util.HashMap;
 public class MyDictionary<K,V> implements IMyDictionary<K,V>
 {
     private final Map<K,V> map;
-    private  K key;
-    private  V value;
-
-
 
     public MyDictionary()
     {
@@ -28,7 +24,7 @@ public class MyDictionary<K,V> implements IMyDictionary<K,V>
     {
         if(!contains(key))
             throw new KeyNotFoundException("The key doesn t exist");
-        for(int i = 0;i < )
+        return this.map.get(key);
     }
 
     @Override
@@ -40,8 +36,20 @@ public class MyDictionary<K,V> implements IMyDictionary<K,V>
     @Override
     public void remove(K key) throws KeyNotFoundException
     {
-        //TO DO
-        return;
+        if(!contains(key))
+            throw new KeyNotFoundException("The key doesn t exist");
+        this.map.remove(key);
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder str = new StringBuilder();
+        for(K key : this.map.keySet())
+        {
+            str.append(key).append("-> ").append(this.map.get(key)).append("\n");
+        }
+        return "My dictionary contains " + str;
     }
 
 }
