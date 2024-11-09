5992c9bbdcda6e5047b3f36b84354999ba80d621
Andreea410 <andreeaungur494@yahoo.com>
2024-11-09 19:01:49 +0200

    added try and catch in the text menu

--- Toy_Language_Interpreter/src/view/TextMenu.java
+++ Toy_Language_Interpreter/src/view/TextMenu.java
@@ -1,14 +1,16 @@
 package view;
 
+import exceptions.EmptyStackException;
 import view.commands.Command;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class TextMenu {
 
-    private Map<String, Command> map;
+    private final Map<String, Command> map;
 
     public TextMenu()
     {
@@ -29,21 +31,27 @@ public class TextMenu {
         }
     }
 
-    public void show()
-    {
+    public void show() throws EmptyStackException, IOException {
         Scanner sc = new Scanner(System.in);
         while(true)
         {
             printMenu();
             System.out.println("Option is: ");
-            String line =  sc.nextLine();
-            Command cm = this.map.get(line);
-            if(cm == null)
-            {
-                System.out.println("Invalid command");
-                continue;
-            }
+         try {
+             String line = sc.nextLine();
+             Command cm = this.map.get(line);
+             if (cm == null) {
+                 System.out.println("Invalid command");
+                 continue;
+             }
+             cm.execute();
+         }
+         catch (EmptyStackException | IOException e)
+         {
+             System.out.println(e.toString());
+         }
         }
+
     }
 
 
